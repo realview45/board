94ee42a53ae01027e8ae292f42cc681e87651218
realview45 <kjink0125@gmail.com>
2026-01-24 18:40:42 +0900

    RestControllerAdvice를 통한 전역예외처리Exception

---
+++ src/main/java/com/beyond/basic/board/common/exception/CommonExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.beyond.basic.board.common.exception;
+
+import com.beyond.basic.board.common.dtos.CommonErrorDto;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CommonExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> illegal(IllegalArgumentException e) {
+        CommonErrorDto dto = CommonErrorDto.builder()
+                .status_code(400)
+                .error_message(e.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(dto);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> notValid(MethodArgumentNotValidException e) {
+        CommonErrorDto dto = CommonErrorDto.builder()
+                .status_code(400)
+                .error_message(e.getFieldError().getDefaultMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(dto);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> noSuch(EntityNotFoundException e) {
+        CommonErrorDto dto = CommonErrorDto.builder()
+                .status_code(404)
+                .error_message(e.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(dto);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> exception(Exception e) {
+        CommonErrorDto dto = CommonErrorDto.builder()
+                .status_code(500)
+                .error_message(e.getMessage())
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(dto);
+    }
+}
